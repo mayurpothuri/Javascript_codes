677cf15d9228055a37665fb96b1a24163500970d
mayurpothuri <mayurpothuri@gmail.com>
2022-07-11 00:39:08 +0530

    added programs and more concepts related to prototype chaining in OOPs in Javascript.

---
+++ OopsJS/prototypeChain.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/*  Note:
+    since every function are objects[first class functions], so it has "function-object" combo.
+    so it has both Object{} and Function{}.
+*/
+function multipleBy2(num) {
+    return num * 2;
+}
+/*  multiplyBy2{
+        multiplyBy2 : function(num){ return num * 2; },
+        __proto__ : Function.prototype
+    }
+*/
+
+console.log(multipleBy2.toString());//this method is in Function.prototype
+/*  Function{
+        prototype : {
+            call : function(...){...},
+            apply : function(...){...},
+            bind : function(...){...},
+            toString : function(...){...}
+            ...
+        }
+        __proto__ : Object.prototype
+    }
+*/
+
+console.log(multipleBy2.hasOwnProperty("score"));//this method is accessible through Object's prototype's object having
+// shared functionalities.
+/*  Object{
+        prototype : {
+            hasOwnProperty : function(...){...},
+            ...
+        }
+        __proto__ : null
+    }
+*/
